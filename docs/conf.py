818352114eb506f1d337792b8e255abf205254ce
Juan Carlos Oviedo <44776372+juancaoviedo@users.noreply.github.com>
2020-08-27 19:42:56 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,9 @@ release = '2.0.0'
 
 # -- General configuration ---------------------------------------------------
 
+# Change contents for index to uploaded to read the docs
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
