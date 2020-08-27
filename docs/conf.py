420b5fc2590baf65e85136def311e2d86d002ab7
Juan Carlos Oviedo <44776372+juancaoviedo@users.noreply.github.com>
2020-08-27 14:19:25 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,10 @@ release = '2.0.0'
 
 # -- General configuration ---------------------------------------------------
 
+# Specify the master document (usually is contents.rst, in here was replaced by index).
+master_doc = 'index'
+
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
